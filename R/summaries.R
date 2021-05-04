6c98678fb679dcb09ddf3f9ba0b935e47d8c2b31
Christopher Barrington <christopher.barrington@crick.ac.uk>
2021-05-04 17:15:38 +0100

    print_object_size

--- R/summaries.R
+++ R/summaries.R
@@ -12,3 +12,17 @@
 #' 
 headtail <- function(x, n=10, n.head=n, n.tail=n)
   rbind(head(x, n=n.head), tail(x, n=n.tail))
+
+#' Get the size of an object
+#' 
+#' Prints the size in \code{units} of an object(s).
+#' 
+#' @param x Object to check
+#' @param unit Units to print
+#' 
+#' @seealso utils:::format.object_size
+#' 
+#' @export
+#' 
+print_object_size <- function(x, unit='GB')
+  utils::object.size(x=x) %>% format(unit=unit[1])

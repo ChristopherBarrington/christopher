d7dac2ca805527e381301977b8549e24491c7720
Christopher Barrington <christopher.barrington@crick.ac.uk>
2021-05-04 17:16:19 +0100

    Update hugo.R

--- R/hugo.R
+++ R/hugo.R
@@ -7,7 +7,14 @@
 #' @return
 #' A character vector for the shortcode to include in the markdown.
 #' 
-#' @export
-#' 
-make_tabs_shortcode <- function()
-  stop('stub')
+make_tabs_shortcode <- function(x, tabset_name='chunk_name') {
+  # {{< tabs "uniqueid" >}}
+  # {{< tab "MacOS" >}} # MacOS Content {{< /tab >}}
+  # {{< tab "Linux" >}} # Linux Content {{< /tab >}}
+  # {{< tab "Windows" >}} # Windows Content {{< /tab >}}
+  # {{< /tabs >}}
+  stop()
+  chunk_name %>% sprintf(fmt='{{< tabs "%s" >}}') -> lines
+
+  lines %>% c('{{< /tabs >}}') %>% str_c(collapse='\n')
+}

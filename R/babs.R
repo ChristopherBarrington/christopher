9865fdef7ea573b72d9e8c17a6fff99513b01c3c
Christopher Barrington <christopher.barrington@crick.ac.uk>
2021-09-20 14:56:06 +0100

    Create babs.R

---
+++ R/babs.R
@@ -0,0 +1,27 @@
+#' Read and parse a .babs file
+#' 
+#' @importFrom datarepository system.file
+#' @importFrom yaml read_yaml
+#' @importFrom purrr pluck
+#' @importFrom purrr map_at
+#' @importFrom purrr when
+#' 
+#' export
+#' 
+read_dotbabs <- function()
+  get_project_root %>%
+    file.path('.babs') %>%
+    when(file.exists(.)~.,
+         TRUE~{sprintf(fmt='no .babs file found at: %s', .) %>% stop(call.=FALSE)}) %>%
+    read_lines() %>%
+    str_replace_all(pattern='\\{.+\\}', replacement='unspecified') %>%
+    read_yaml(text=.) %>%
+    pluck(1) %>%
+    map_at('Scientist', str_remove, pattern='@crick.ac.uk')
+
+get_project_root <- function()
+  getwd() %>%
+    str_split('/') %>%
+    unlist() %>%
+    head(n=10) %>%
+    str_c(collapse='/')

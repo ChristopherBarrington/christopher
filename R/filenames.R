61c796a53e6a9805dd2dde83629affa5c772341f
Christopher Barrington <christopher.barrington@crick.ac.uk>
2021-10-12 11:08:25 +0100

    Update filenames.R

--- R/filenames.R
+++ R/filenames.R
@@ -8,3 +8,31 @@
 #' 
 get_file_extension <- function(path)
   str_remove(string=path, pattern='.*\\.')
+
+#' Create and/or clear out a directory
+#' 
+#' @param path Path to directory to create and/or clean
+#' @param clean Should the contents of `path` be deleted? Defaults to `FALSE` for safety.
+#' 
+#' @return
+#' Invisibly returns the `path` that may have been created and/or cleaned.
+#' 
+#' @export
+#' 
+make_clean_directory <- function(path, clean=FALSE) {
+  # check that there is something to do
+  if(missing(path))
+    stop('no path was specified!', call.=FALSE)
+
+  if(!is.logical(clean))
+    stop('clean must be either TRUE or FALSE')
+
+  # create the directory, even if it exists don't moan about it
+  dir.create(path=path, recursive=TRUE, showWarnings=FALSE)
+
+  # if cleaning is needed, do it
+  if(clean)
+    sprintf('rm -r %s/*', path) %>% system()
+
+  invisible(path)
+}

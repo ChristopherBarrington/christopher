aab99ea485583a2f747de1f03209e702ce3c9ce0
Christopher Barrington <christopher.barrington@crick.ac.uk>
2021-10-12 11:47:06 +0100

    added slurm.R

---
+++ R/slurm.R
@@ -0,0 +1,35 @@
+
+#' Write parameters file
+#' 
+#' Writes a `data.frame` as a `csv` ready to be ready by an `sbatch` job array.
+#' 
+#' @param x `data.frame` of parameters
+#' @param filename Path to output file
+#' @param add_id Should an `id` variable be included, which is just a zero-padded row number
+#' 
+#' @importFrom dplyr mutate n
+#' @importFrom readr write_csv
+#' @importFrom stringr str_flatten str_pad str_to_upper
+#' 
+#' @export
+#' 
+write_parameters_file <- function(x, filename='parameters.csv', add_id=TRUE) {
+  # get the function with with a file will be written
+  fmt <- 'csv' # maybe make this an argument?
+  write_fmt <- get('write_csv')
+  # if(missing(fmt) %>% not())
+  #   filename %<>% fs::path(ext=fmt)
+
+  # add a row number as a column
+  if(add_id)
+    x %<>%
+      mutate(id={n() %>% seq() %>% str_pad(width={nchar(.) %>% max()}, pad='0', side='left')},
+             .before=1)
+
+  # write the contents of x to file
+  write_fmt(x=x, file=filename, col_names=FALSE)
+  sprintf(fmt='#SBATCH --array 1-%d:1', nrow(x)) %>% message()
+  sprintf(fmt="IFS=',' read %s <<< $(awk \"NR==${SLURM_ARRAY_TASK_ID}{print}\" %s)", {colnames(x) %>% str_flatten(collapse=' ') %>% str_to_upper()}, basename(filename)) %>% message()
+
+  invisible(filename)
+}

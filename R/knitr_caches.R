7f414de90fac918f9e39e3712421779b4eec4d58
Christopher Barrington <christopher.barrington@crick.ac.uk>
2022-03-11 09:27:18 +0000

    Update knitr_caches.R
    remove lazyload function replaced with general chunk loading

--- R/knitr_caches.R
+++ R/knitr_caches.R
@@ -1,45 +1,71 @@
-#' Lazy load a knitr chunk
+#' Load a cached knitr chunk
+#' 
+#' Loads the cached chunk from cache using lazy loading where possible.
 #'
-#' @param chunk Name of cached chunk (from Rmd). Can be a path to a chunk (partial), from which `path` will be taken,
-#' @param cache Root path within which cached (`*rdx`) are recursively searched
+#' @param chunk Name of cached chunk (from Rmd). Can be a path to a chunk (partial), from which `path` will be taken.
+#' @param cache Root path to cache(s) that will be searched for `chunk`
 #' @param filter Character vector of object names to retain
 #' @param env Environment into which objects are loaded
 #'
-#' @describeIn lazyload_cached_chunk Lazy load a cached chunk
+#' @describeIn load_cached_chunk Load a cached chunk
+#' 
+#' @importFrom magrittr %T>%
+#' @importFrom purrr when
+#' @importFrom stringr str_detect str_remove
 #' 
 #' @export
 #' 
-lazyload_cached_chunk <- function(chunk='unnamed-chunk', cache=c('knitr_cache', 'cache'), filter, env=globalenv()) {
-  chunk <- basename(chunk)
-  chunk_file <- list.files(path=cache, pattern=sprintf('%s.*.rdx', chunk), full.names=TRUE, recursive=TRUE)
-  chunk_file <- stringr::str_remove(chunk_file, pattern='.rdx')
+load_cached_chunk <- function(chunk='unnamed-chunk', cache=c('knitr_cache', 'cache'), filter, env=globalenv()) {
+  # get the potential chunks from the cache
+  list.files(path=cache, pattern=chunk, full.names=TRUE) -> chunks_list
 
-  if(length(chunk_file) > 1)
-    stop('Cannot load chunk from:\n', paste(chunk_file, collapse='\n'))
-  if(length(chunk_file) == 0)
-    stop('No chunks called `', chunk, '` found!')
+  # check for the number of files for the chunk
+  chunks_list %>%
+    when(length(.)==0~stop('[load_cached_chunk] No cached chunks found for: ', chunk, call.=FALSE),
+         length(.)==1 && str_detect(., '\\.(RData)$')~'load',
+         length(.)==3 && {str_detect(., '\\.(RData|rdb|rdx)$') %>% all()}~'lazy', # this may not work as expected
+         length(.)>3~stop('[load_cached_chunk] Too many potential chunks for: ', chunk, call.=FALSE),
+         TRUE~stop('[load_cached_chunk] Cannot load (may be incomplete?) chunk: ', chunk, call.=FALSE)) -> cache_type
 
-  filter_func <- function(x) rep(TRUE, length(x))
-  if(!missing(filter))
-    filter_func <- function(x) x %in% filter
+  # load the chunk
+  if(cache_type=='load')
+    chunks_list %>%
+      head(n=1) %T>%
+      load(envir=env) %>%
+      remove_extension(filename_only=FALSE) -> chunk_file
 
-  lazyLoad(chunk_file, envir=env, filter=filter_func)
+  # lazily load the chunk
+  if(cache_type=='lazy') {
+    chunks_list %>%
+      head(n=1) %>%
+      remove_extension(filename_only=FALSE) -> chunk_file
+
+    filter_func <- function(x) rep(TRUE, length(x))
+    if(!missing(filter))
+      filter_func <- function(x) x %in% filter
+
+    lazyLoad(chunk_file, envir=env, filter=filter_func)
+  }
+
+  # return the path of the loaded chunk
   invisible(chunk_file)
 }
 
 #' Lazy load all chunks in a cache
 #' 
-#' @param cache Path to the cache from which all chunks are lazily loaded
+#' @param cache Path to the cache from which all chunks are loaded
 #' 
 #' @details
 #' Any existing objects in the environment will be overwritten when chunk(s) are loaded. 
 #' 
-#' @describeIn lazyload_cached_chunk Lazy load all cached chunks (alphabetically)
+#' @describeIn load_cached_chunk Load all cached chunks (alphabetically)
+#' 
+#' importFrom plyr l_ply
 #' 
 #' @export
 #' 
 load_all_chunks <- function(cache=c('knitr_cache', 'cache'))
   list.files(cache, pattern='*RData') %>%
     str_remove('.RData') %>%
-    plyr::l_ply(lazyload_cached_chunk, cache=cache)
+    l_ply(load_cached_chunk, cache=cache)
 
